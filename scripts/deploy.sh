1655ddc9ef5c602ae90bea761aa076d341906dd0
mjubenot <thiev42@gmail.com>
2018-06-28 11:25:17 +0200

    Modifying the CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,3 @@ echo $SSH_USER
 echo $SSH_ENDPOINT
 echo $SSH_PORT
 sshpass -p '$SSH_PASS' ssh $SSH_USER@$SSH_ENDPOINT -p $SSH_PORT "touch hey"
-echo hi
-sshpass -p '$SSH_PASS' ssh $SSH_USER@$SSH_ENDPOINT -p $SSH_PORT "$DEPLOY_PATH/bin/asadmin undeploy AtlantisJEE"
-echo hi
-sshpass -p '$SSH_PASS' scp -P $SSH_PORT target/AtlantisJEE.war $SSH_USER@$SSH_ENDPOINT:$DEPLOY_PATH
-echo hi
-sshpass -p '$SSH_PASS' ssh $SSH_USER@$SSH_ENDPOINT -p $SSH_PORT "$DEPLOY_PATH/bin/asadmin deploy $DEPLOY_PATH/AtlantisJEE.war"
-echo hi
