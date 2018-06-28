e9bc40a9169ebcfde8fbfe2da8be4dc6f7433c25
mjubenot <thiev42@gmail.com>
2018-06-28 12:00:49 +0200

    Modifying the CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ echo $SSH_PASS
 echo $SSH_USER
 echo $SSH_ENDPOINT
 echo $SSH_PORT
-sshpass -p '$SSH_PASS' ssh $SSH_USER@$SSH_ENDPOINT -p $SSH_PORT "touch hey"
+sudo sshpass -p '$SSH_PASS' ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_ENDPOINT -p $SSH_PORT touch hey
